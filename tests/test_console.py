877091c9ce638a60cab23f8261507c757cc4e11c
Vince Broz <vince.broz@bluelabs.com>
2020-06-18 12:41:50 -0400

    Refactor and add testing via mocks

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,4 +1,5 @@
 import click.testing
+import requests
 import pytest
 
 from hypermodern_python_vb import console
@@ -9,6 +10,44 @@ def runner():
     return click.testing.CliRunner()
 
 
-def test_main_succeeds(runner):
+def test_main_succeeds(runner, mock_requests_get):
+    result = runner.invoke(console.main)
+    assert result.exit_code == 0
+
+
+def test_main_prints_title(runner, mock_requests_get):
+    result = runner.invoke(console.main)
+    assert "Lorem Ipsum" in result.output
+
+
+def test_main_invokes_requests_get(runner, mock_requests_get):
+    runner.invoke(console.main)
+    assert mock_requests_get.called
+
+
+def test_main_fails_on_request_error(runner, mock_requests_get):
+    mock_requests_get.side_effect = Exception("Boom")
+    result = runner.invoke(console.main)
+    assert result.exit_code == 1
+
+
+def test_main_prints_message_on_request_error(runner, mock_requests_get):
+    mock_requests_get.side_effect = requests.RequestException
+    result = runner.invoke(console.main)
+    assert "Error" in result.output
+
+
+@pytest.fixture
+def mock_wikipedia_random_page(mocker):
+    return mocker.patch("hypermodern_python_vb.wikipedia.random_page")
+
+
+def test_main_uses_specified_language(runner, mock_wikipedia_random_page):
+    runner.invoke(console.main, ["--language=pl"])
+    mock_wikipedia_random_page.assert_called_with(language="pl")
+
+
+@pytest.mark.e2e
+def test_main_succeeds_in_production_env(runner):
     result = runner.invoke(console.main)
     assert result.exit_code == 0
