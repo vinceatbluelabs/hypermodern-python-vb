f4155ec3eaccb2c513ab3e5e942442bc53525fdb
Vince Broz <vince.broz@bluelabs.com>
2020-06-18 14:39:17 -0400

    Add flake8 and black

--- noxfile.py
+++ noxfile.py
@@ -5,4 +5,21 @@ import nox
 def tests(session):
     args = session.posargs or ["--cov", "-m", "not e2e"]
     session.run("poetry", "install", external=True)
-    session.run("pytest", "--cov")
+    session.run("pytest", *args)
+
+
+locations = "src", "tests", "noxfile.py"
+
+
+@nox.session(python=["3.8", "3.7"])
+def lint(session):
+    args = session.posargs or locations
+    session.install("flake8")
+    session.run("flake8", *args)
+
+
+@nox.session(python="3.8")
+def black(session):
+    args = session.posargs or locations
+    session.install("black")
+    session.run("black", *args)
